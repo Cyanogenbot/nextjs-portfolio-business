3a17a383b7cd2212b33a15cce18b25e6fd3e2deb
Diede van Marle <contact@designedbydie.de>
2024-06-17 22:43:13 +0200

    removed svg

--- pages/index.jsx
+++ pages/index.jsx
@@ -58,13 +58,6 @@ const future={
 
 }
 
-function logoIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />{" "}
-    </SvgIcon>
-  );
-}
 export default function Home() {
   return (
     <Layout home>
